5342835df6deeb50c22fc950dbd4c40b47b68e0b
Eric Hainer <eric.hainer@gmail.com>
2018-09-12 20:40:50 -0600

    [FEAT] Add shortcodes plugin

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.10.0'
+lock '~> 3.11.0'
 
 set :application, 'killjoyranch'
 set :repo_url, 'git@github.com:ehainer/killjoyranch.git'
